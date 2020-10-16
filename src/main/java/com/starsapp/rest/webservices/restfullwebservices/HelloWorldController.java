f8dd2b1fb1c72cb3bdee397ef265dd1d3761e423
OTHMANI <najmothmani@gmail.com>
2020-10-16 13:58:27 +0200

    Implement Restful Api

---
+++ src/main/java/com/starsapp/rest/webservices/restfullwebservices/HelloWorldController.java
@@ -0,0 +1,31 @@
+package com.starsapp.rest.webservices.restfullwebservices;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class HelloWorldController {
+
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        try {
+            return new HelloWorldBean("Hello World test automake done....");
+        } catch (RuntimeException e) {
+            throw new RuntimeException("some error has happened! contact at najm@gmail.com ", e);
+        }
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+}
