ba3ddf365dbdb1bd29b9cc6517ad98ce145650fc
OTHMANI <najmothmani@gmail.com>
2020-11-08 17:22:23 +0100

    setUp Spring security
    Signed-off-by: OTHMANI <najmothmani@gmail.com>

---
+++ src/main/java/com/starsapp/rest/webservices/restfullwebservices/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -0,0 +1,20 @@
+package com.starsapp.rest.webservices.restfullwebservices.basic.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+    }
+}
