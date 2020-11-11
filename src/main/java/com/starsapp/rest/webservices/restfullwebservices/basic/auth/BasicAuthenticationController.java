eb545a4fdfb46bd8b6663b00e0acc9a269067dcc
OTHMANI <najmothmani@gmail.com>
2020-11-11 13:23:17 +0100

    -Fix SpringSecurity BasicAuth Configuration -Add BasicAuth Controller & Bean
    Signed-off-by: OTHMANI <najmothmani@gmail.com>

---
+++ src/main/java/com/starsapp/rest/webservices/restfullwebservices/basic/auth/BasicAuthenticationController.java
@@ -0,0 +1,14 @@
+package com.starsapp.rest.webservices.restfullwebservices.basic.auth;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class BasicAuthenticationController {
+    @GetMapping(path = "/basicauth")
+    public AuthenticationBean authenticationBean() {
+        return new AuthenticationBean("you are authenticated");
+    }
+}
--- src/main/java/com/starsapp/rest/webservices/restfullwebservices/basic/auth/SpringSecurityConfigurationBasicAuth.java
+++ src/main/java/com/starsapp/rest/webservices/restfullwebservices/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -5,16 +5,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
+    protected void configure(HttpSecurity http)
+            throws Exception {
+        http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/**").permitAll()
-                .anyRequest().authenticated()
-                .and().httpBasic();
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 }
